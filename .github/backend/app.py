ee9d206eb628746a0db2953503ee7db657d3df53
WixPartner <202474862+WixPartner@users.noreply.github.com>
2025-03-28 02:21:01 -0700

    Update app.py
    @WixPartner

--- .github/backend/app.py
+++ .github/backend/app.py
@@ -1,22 +1,5 @@
 from flask import Flask, request, jsonify
-from search import search_index
-
-app = Flask(__name__)
-
-@app.route('/status', methods=['GET'])
-def status():
-    return jsonify({'status': 'running'}), 200
-
-@app.route('/search', methods=['GET'])
-def catagory():
-    query = request.args.get('query', '')
-    results = catagory_index(query)
-    return jsonify({'results': results}), 200
-
-if __name__ == '__main__':
-    app.run(debug=True)
-from flask import Flask, request, jsonify
-from search import search_index
+from search import search_index, catagory_index
 
 app = Flask(__name__)
 
@@ -28,6 +11,12 @@ def status():
 def search():
     query = request.args.get('query', '')
     results = search_index(query)
+    return jsonify({'results': results}), 200
+
+@app.route('/category', methods=['GET'])
+def category():
+    query = request.args.get('query', '')
+    results = catagory_index(query)
     return jsonify({'results': results}), 200
 
 if __name__ == '__main__':
